d9e9fba77f9e46ad778b1fd64426f4a6ffdc29b2
Asep Sutisna <sutisnaasep323@gmail.com>
2021-08-16 11:36:13 +0700

    Dicoding_BFAA_MyUnitTesting_Add class MainViewModel

---
+++ app/src/main/java/com/example/myunittesting/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.myunittesting
+
+class MainViewModel(private val cuboidModel : CuboidModel) {
+
+    fun getCircumference(): Double = cuboidModel.getCircumference()
+    fun getSurfaceArea(): Double = cuboidModel.getSurfaceArea()
+    fun getVolume(): Double = cuboidModel.getVolume()
+
+    fun save(l : Double, w : Double, h: Double){ //deklarasi
+        cuboidModel.save(l,w,h) // inisialisasi, ambil dari Class CuboidModel
+    }
+
+}
